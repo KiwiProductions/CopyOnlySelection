706086bf097bd3ef3a2297e74553e447fed5b83a
kiwiproductions <kiwi@kiwiproductionsllc.com>
2019-06-16 17:34:46 -0400

    Initial version

---
+++ CopyOnlySelectionCommand.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyOnlySelection
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CopyOnlySelectionCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("6311f768-cd9e-42be-b2cd-2ac680f918de");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyOnlySelectionCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CopyOnlySelectionCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CopyOnlySelectionCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CopyOnlySelectionCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CopyOnlySelectionCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            IWpfTextView textView = Helpers.GetCurentTextView();
+
+            DTE2 dte = Helpers.GetDTE();
+
+            // Only block copy / cut if the active window is a document window, and the selection is empty.
+            if (textView == null)
+            {
+                Debug.WriteLine("Could not find IWpfTextView");
+                dte.ExecuteCommand("Edit.Copy");
+                return;
+            }
+
+            ITextSnapshot snapshot = textView.TextSnapshot;
+            if (snapshot != snapshot.TextBuffer.CurrentSnapshot) return;
+            if (dte.ActiveWindow.Type == EnvDTE.vsWindowType.vsWindowTypeDocument && textView.Selection.IsEmpty) return;
+            dte.ExecuteCommand("Edit.Copy");
+        }
+    }
+}
---
+++ Helpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace CopyOnlySelection
+{
+    static class Helpers
+    {
+
+        private static DTE2 _dte = null;
+        public static DTE2 GetDTE()
+        {
+            if (_dte == null)
+            {
+                _dte = Package.GetGlobalService(typeof(SDTE)) as DTE2;
+            }
+            return _dte;
+        }
+
+        public static IWpfTextView GetCurentTextView()
+        {
+            var componentModel = GetComponentModel();
+            if (componentModel == null) return null;
+            var vsTextView = GetCurrentNativeTextView();
+            if (vsTextView == null) return null;
+            var editorAdapter = componentModel.GetService<IVsEditorAdaptersFactoryService>();
+            return editorAdapter.GetWpfTextView(vsTextView);
+        }
+
+        public static IVsTextView GetCurrentNativeTextView()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var textManager = (IVsTextManager)ServiceProvider.GlobalProvider.GetService(typeof(SVsTextManager));
+            Assumes.Present(textManager);
+            IVsTextView activeView = null;
+            textManager.GetActiveView(1, null, out activeView);
+            return activeView;
+        }
+
+        public static IComponentModel GetComponentModel()
+        {
+            return (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+        }
+    }
+}
